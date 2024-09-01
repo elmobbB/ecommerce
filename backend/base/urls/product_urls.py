77f21f6c543756389081f4fbb18206445b82b002
ClairePoon <pnthaha@gmail.com>
2024-09-01 17:21:27 +0800

    add product review endpoint and implementation on screen

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from base.views import product_views as views 
 
 urlpatterns = [
-    #api/XXXX
+
+    #/api/products/:id/reviews/
     path('', views.getProducts, name='products'),
 
     path('create/', views.createProduct, name='product-create'),# need o move this up , becuase it's one param , it's gonna think it's a dynamic value
     path('upload/', views.uploadImage, name='image-upload'),# need o move this up , becuase it's one param , it's gonna think it's a dynamic value
+
+    path('<str:pk>/reviews/', views.createProductReview, name='create-review'),
     path('<str:pk>', views.getProduct, name='product'),
     
     path('update/<str:pk>/', views.updateProduct, name='product-delete'),
